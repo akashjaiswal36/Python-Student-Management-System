81d4edc88e7a836a5e9b08525db47eb23334a5a3
Akash Jaiswal <akash.jaiswal@colruytgroup.com>
2025-07-01 01:25:31 +0200

    added codes

--- src/main.py
+++ src/main.py
@@ -3,8 +3,12 @@ from fastapi.staticfiles import StaticFiles
 from .database import Base, engine
 from .routers import students
 
-Base.metadata.create_all(bind=engine)     # create table if not exist
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Student Management System")
-app.mount("/", StaticFiles(directory="app/static", html=True), name="static")
+
+# Mount the static files relative to this file
+app.mount("/", StaticFiles(directory="src/static", html=True), name="static")
+
+# Include API routes
 app.include_router(students.router)
